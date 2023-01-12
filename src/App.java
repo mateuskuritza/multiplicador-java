fb6d239935013e528c832a87e2f4d0a22bdd887f
mateuskuritza <mateuskuritza@gmail.com>
2023-01-12 11:26:49 -0300

    show number multiplication table

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int number = 5;
+
+        System.out.println("Tabela de multiplicação de " + number);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " X " + i + " = " + number*i);
+        }
     }
 }
